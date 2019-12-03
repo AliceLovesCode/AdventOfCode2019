16fb93bdab4a83d86cc598ccc747883642ec8f13
AliceLovesCode <alice@aliceisabelle.com>
2019-12-03 14:48:54 -0600

    Add Day 1 Part 2 functionality

--- day1.py
+++ day1.py
@@ -2,16 +2,32 @@ import math
 import sys
 from pathlib import Path
 
-def module_fuel_required(module_mass):
-    return math.floor(module_mass / 3) - 2
+def mass_fuel_required(mass):
+    return math.floor(mass / 3) - 2
 
-def total_fuel_required(module_masses):
-    module_fuel_quantities = [module_fuel_required(x) for x in module_masses]
+def part1_summed_fuel_required(module_masses):
+    module_fuel_quantities = [mass_fuel_required(x) for x in module_masses]
     return sum(module_fuel_quantities)
 
+def part2_summed_fuel_required(module_masses):
+    module_fuel_quantities = [recursive_fuel_required(x) for x in module_masses]
+    return sum(module_fuel_quantities)
+
+def recursive_fuel_required(mass):
+    fuel_required = mass_fuel_required(mass)
+    if fuel_required <= 0:
+        return 0
+    else: 
+        return fuel_required + recursive_fuel_required(fuel_required)
+    
+
 def load_masses_file(filename):
-    file_data = [int(x) for x in Path(filename).read_text().split('\n')]
-    return total_fuel_required(file_data)
+    return [int(x) for x in Path(filename).read_text().split('\n')]
+
+def do_parts(input_file):
+    masses = load_masses_file(input_file)
+    print("Day 1 Part 1 fuel calculation: {0}\n".format(part1_summed_fuel_required(masses)))
+    print("Day 1 Part 2 fuel calculation: {0}\n".format(part2_summed_fuel_required(masses)))
 
 if __name__ == '__main__':
-    print(load_masses_file(sys.argv[1]))
+    do_parts(sys.argv[1])
--- tests.py
+++ tests.py
@@ -6,12 +6,18 @@ class TestAdvent(unittest.TestCase):
         checklist = [(12, 2), (14, 2), (1969, 654), (100756, 33583)]
         for check in checklist:
             with self.subTest(check=check):
-                self.assertEqual(day1.module_fuel_required(check[0]), check[1])
+                self.assertEqual(day1.mass_fuel_required(check[0]), check[1])
 
     def test_day1_total_fuel(self):
         checklist = [12, 14, 1969]
-        self.assertEqual(day1.total_fuel_required(checklist), 658)
+        self.assertEqual(day1.part1_summed_fuel_required(checklist), 658)
     
+    def test_day1_recursive_fuel(self):
+        checklist = [(1969, 966), (100756, 50346)]
+        for check in checklist:
+            with self.subTest(check=check):
+                self.assertEqual(day1.recursive_fuel_required(check[0]), check[1])
+        
 
 if __name__ == '__main__':
     unittest.main()
