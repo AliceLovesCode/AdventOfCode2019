4fefc7687dc6c46fe8090cfb73b50f2cab3b2134
AliceLovesCode <alice@aliceisabelle.com>
2019-12-04 15:11:40 -0600

    Minor additional day 3 work

--- day3.py
+++ day3.py
@@ -8,12 +8,22 @@ def manhattan_distance(coord1, coord2):
     return abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
 
 class WireBoard:
-    board = []
+    board = [[[],[],[]],[[],[],[]],[[],[],[]]]
     def __init__(self, description):
-        board = [[[],[],[]],[[],[],[]],[[],[],[]]]
+        self.board = self.parse_description(description)
+
+    def parse_description(self, description):
+        wires = description.split('\n')
+        segments = [x.split(',') for x in wires]
+        print(segments)
+        return segments
+
+    def calculate_board_extent(self, segments):
+        print('no')
 
     def __str__(self):
-        return str(board)
+        return str(self.board)
 
 if __name__ == '__main__':
-    print(sys.argv[1])
+    board = WireBoard("R8,U5,L5,D3\nU7,R6,D4,L4")
+    print(board)
